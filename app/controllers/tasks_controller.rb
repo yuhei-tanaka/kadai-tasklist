9f234e1b931b2d95d7ccde0f8d45bc3dfd97ef76
yuhei.tanaka <everythingok.9292@gmail.com>
2020-04-30 11:30:44 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
     @tasks = Task.all
@@ -13,14 +15,15 @@ class TasksController < ApplicationController
   end
 	  
   def create  
-	  @task = Task.new(task_params)
-	    if @task.save
-	      flash[:success] = 'Task が正常に登録されました'
-	      redirect_to @task
-	    else
-	      flash.now[:danger] = 'Task が登録されませんでした'
-	      render:new
-	    end
+	  @task = current_user.tasks.build(task_params)
+      if @task.save
+			  flash[:success] = 'メッセージを投稿しました。'
+			  redirect_to root_url
+      else
+			  @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+			  flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+			  render root_url
+      end
   end
 	  
   def edit
@@ -38,8 +41,8 @@ class TasksController < ApplicationController
 	  
   def destroy 
     @task.destroy
-      flash[:success] = 'Task は正常に削除されました'
-	    redirect_to tasks_url
+		flash[:success] = 'メッセージを削除しました。'
+		redirect_back(fallback_location: root_path)
   end 
 
 end
@@ -53,4 +56,11 @@ end
   
 def set_task
   @task = Task.find(params[:id])
+end
+
+def correct_user
+	@task = current_user.tasks.find_by(id: params[:id])
+	unless @task
+		redirect_to root_url
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 	post 'login', to: 'sessions#create'
 	delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
+  resources :users, only: [:new, :create]
   resources :tasks
 end
