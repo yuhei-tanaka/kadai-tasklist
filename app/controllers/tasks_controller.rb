0d5d1921f2226b707ffbf5697e748d46823e57b5
yuhei.tanaka <everythingok.9292@gmail.com>
2020-05-03 02:45:47 +0000

    post3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,10 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, except: [:index]
-  before_action :correct_user, only: [:destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update,:destroy]
   
   def index
     if logged_in?
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    else
-      redirect_to login_path
     end
   end
 	  
@@ -59,10 +56,6 @@ def task_params
   params.require(:task).permit(:content, :status)
 end
   
-def set_task
-  @task = Task.find(params[:id])
-end
-
 def correct_user
 	@task = current_user.tasks.find_by(id: params[:id])
 	unless @task
